3a6e0d9cbd44246ca274f78218ccf6bd279eeb3b
Bintaaaa <bijantium.yt.32@gmail.com>
2022-02-02 03:27:14 +0700

    add service in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Service from "./components/service/Service";
 import TopBanner from "./components/topBanner/TopBanner";
 function App() {
   return (
     <div>
       <TopBanner />
+      <Service />
     </div>
   );
 }
