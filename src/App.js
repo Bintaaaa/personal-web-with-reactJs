248c3bcedebb8d5b7031e44fc9eb52cb49274301
Bintaaaa <bijantium.yt.32@gmail.com>
2022-02-03 03:03:08 +0700

    register component analysist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Analisyst from "./components/analisyst/Analisyst";
 import Service from "./components/service/Service";
 import TopBanner from "./components/topBanner/TopBanner";
 import TopNavigation from "./components/topNavigation/TopNavigation";
@@ -8,6 +9,7 @@ function App() {
       <TopNavigation />
       <TopBanner />
       <Service />
+      <Analisyst />
     </div>
   );
 }
