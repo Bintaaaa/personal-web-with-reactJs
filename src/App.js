42ab23ce530719dc4fcfb9a7f9a50a337a67edf7
Bintaaaa <bijantium.yt.32@gmail.com>
2022-02-03 02:27:37 +0700

    register TopNavigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Service from "./components/service/Service";
 import TopBanner from "./components/topBanner/TopBanner";
+import TopNavigation from "./components/topNavigation/TopNavigation";
 function App() {
   return (
     <div>
+      <TopNavigation />
       <TopBanner />
       <Service />
     </div>
