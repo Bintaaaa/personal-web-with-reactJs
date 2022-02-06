7de8a532db292c22f4d0fa4be076f0ac152f5195
Bintaaaa <bijantium.yt.32@gmail.com>
2022-02-06 23:44:27 +0700

    Create About.jsx

---
+++ src/components/about/About.jsx
@@ -0,0 +1,38 @@
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import face from '../../assets/images/face.png'
+import { init } from 'ityped'
+class About extends Component {
+    componentDidMount() {
+        const myElement = document.querySelector('#myElement')
+        init(myElement, { showCursor: false, strings: ['Web Developer!', 'Online Instructor!'] })
+    }
+    render() {
+        return (
+            <Fragment>
+                <Container>
+                    <div className="text-center">
+                        <h1 className='serviceTitle'>About Me</h1>
+                        <div className="underlineOfServiceTitle"></div>
+                    </div>
+                    <Row>
+                        <Col lg={6} md={6} sm={12}>
+                            <div className="aboutImage">
+                                <img className='aboutMeImage' src={face} alt="" />
+                            </div>
+                        </Col>
+                        <Col lg={6} md={6} sm={12}>
+                            <div className="aboutMeBody">
+                                <h2 className="aboutMeDetails">HI There, I'm</h2>
+                                <h2 className="aboutMeTitle">Bijantyum</h2>
+                                <h3 className="aboutMeDetails">Work as <span id="myElement"> </span> </h3>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
+}
+
+export default About
