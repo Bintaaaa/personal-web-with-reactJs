45f72b74373d1ac5c80ecffd4dbf451615f571b9
Bintaaaa <bijantium.yt.32@gmail.com>
2022-03-02 00:06:51 +0700

    Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,18 +3,17 @@ import { Fragment } from 'react'
 import About from "../components/about/About";
 import Analisyst from "../components/analisyst/Analisyst";
 import Course from "../components/course/Course";
-import Footer from "../components/footer/Footer";
 import RecentProject from "../components/recent/RecentProject";
 import ReviewClient from "../components/review/ReviewClient";
 import Service from "../components/service/Service";
 import Summery from "../components/summery/Summery";
 import TopBanner from "../components/topBanner/TopBanner";
-import TopNavigation from "../components/topNavigation/TopNavigation";
 import Video from "../components/video/Video";
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
+                <TopBanner />
                 <Service />
                 <Analisyst />
                 <Summery />
